21c6bb4a5f5e1a615cad8cf1841d55cb57af6dbe
518405 <518405@PC224405.cts.com>
2017-06-06 17:24:19 +0530

    Lambda Expression

---
+++ src/main/java/com/java8/learning/lambda/LambdaLearning.java
@@ -0,0 +1,30 @@
+package com.java8.learning.lambda;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaLearning {
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
+
+		Collections.sort(names, new Comparator<String>() {
+			@Override
+			public int compare(String a, String b) {
+				return b.compareTo(a);
+			}
+		});
+
+		Collections.sort(names, (String a, String b) -> {
+			return b.compareTo(a);
+		});
+
+		Collections.sort(names, (String a, String b) -> b.compareTo(a));
+		Collections.sort(names, (a, b) -> b.compareTo(a));
+		System.out.println(names);
+		
+		names.sort((a, b) -> b.compareTo(a));
+		System.out.println(names);
+	}
+}
