dd7b3ccf451007bd01113fd033cfab85fe0c2f76
HK\mazin m ismail <mazin_m_ismail@manulife.com>
2017-11-23 17:07:16 +0800

    Adding puzzles

---
+++ src/main/puzzles/com/puzzles/string/CountCharacterOccurence.java
@@ -0,0 +1,10 @@
+package com.puzzles.string;
+
+class CountCharacterOccurence {
+	public static void main(String[] args) {
+		String s = "Java is java again java again";
+		String c = "a";
+		int count = s.length() - s.replace(c, "").length();
+		System.out.println("Number of occurances of 'a' in " + s + " = " + count);
+	}
+}
