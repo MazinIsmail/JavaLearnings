3a29db30de5370f4735e082afe089486e5f1ca7e
Mazin Ismail <ismail.mazin@gmail.com>
2018-09-19 14:01:15 +0530

    Enum Singleton

---
+++ src/main/designpattern/com/pattern/singleton/EnumSingleton.java
@@ -0,0 +1,11 @@
+package com.pattern.singleton;
+
+/*Reflection can be used to destroy singleton implementation approaches.
+ * To overcome this situation with Reflection we can use Enum Singleton.
+ * The drawback is that the enum type is somewhat inflexible; for example, it does not allow lazy initialization.*/
+public enum EnumSingleton {
+	INSTANCE;
+	public static void doSomething() {
+		// do something
+	}
+}
