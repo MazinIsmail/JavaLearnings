c863b9f9fe6d1e447024e294c5e8019bb62abcf8
Mazin Ismail <ismail.mazin@gmail.com>
2018-09-20 11:28:20 +0530

    AbstractFactoryPattern

---
+++ src/main/designpattern/com/pattern/abstractfactory/AbstractFactoryPatternDemo.java
@@ -0,0 +1,17 @@
+package com.pattern.abstractfactory;
+
+/* Creational pattern and almost similar to Factory Pattern except the fact that it’s more like factory of factories.
+ * Abstract Factory pattern is robust and avoid conditional logic of Factory pattern.*/
+public class AbstractFactoryPatternDemo {
+
+	public static void main(String[] args) {
+		testAbstractFactory();
+	}
+
+	private static void testAbstractFactory() {
+		Computer pc = ComputerFactory.getComputer(new PCFactory("2 GB", "500 GB", "2.4 GHz"));
+		Computer server = ComputerFactory.getComputer(new ServerFactory("16 GB", "1 TB", "2.9 GHz"));
+		System.out.println("AbstractFactory PC Config :: " + pc);
+		System.out.println("AbstractFactory Server Config :: " + server);
+	}
+}
--- src/main/designpattern/com/pattern/factory/Shape.java
+++ src/main/designpattern/com/pattern/factory/Shape.java
@@ -1,5 +1,6 @@
 package com.pattern.factory;
 
+/*Super class in factory pattern can be an interface, abstract class or a normal java class.*/
 public interface Shape {
 	void draw();
 }
