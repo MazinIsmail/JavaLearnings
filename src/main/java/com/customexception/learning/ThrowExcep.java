76ceb2fc886d4d873a7c8e11fbc568af366039c8
SU332095 <SU332095@D-113155585.wipro.com>
2019-06-12 12:34:04 +0530

    throw and nested try

--- src/main/java/com/customexception/learning/ThrowExcep.java
+++ src/main/java/com/customexception/learning/ThrowExcep.java
@@ -3,19 +3,15 @@ package com.customexception.learning;
 //The throw keyword in Java is used to explicitly throw an exception from a method or any block of code.
 
 class ThrowExcep 
-{ 
-	static void checkEligibilty(int stuage, int stuweight){ 
-	      if(stuage<12 && stuweight<40) {
-	         throw new ArithmeticException("Student is not eligible for registration"); 
-	      }
-	      else {
-	         System.out.println("Student Entry is Valid!!"); 
-	      }
-	   } 
-
-	   public static void main(String args[]){ 
-	     System.out.println("Welcome to the Registration process!!");
-	     checkEligibilty(10, 39); 
-	     System.out.println("Have a nice day.."); 
-	 } 
-	}
+{
+static void validate(int age){  
+    if(age<18)  
+     throw new ArithmeticException("not valid");  
+    else  
+     System.out.println("welcome to vote");  
+  }  
+  public static void main(String args[]){  
+     validate(13);  
+     System.out.println("rest of the code...");  
+ }  
+} 
