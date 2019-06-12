dc105ab679c2d36c403925d298875e8229e534b8
SU332095 <SU332095@D-113155585.wipro.com>
2019-06-12 12:35:30 +0530

    multiple catch

--- src/main/java/com/customexception/learning/MultipleCatch.java
+++ src/main/java/com/customexception/learning/MultipleCatch.java
@@ -12,7 +12,7 @@ public class MultipleCatch {
 		try {
 			int a[] = new int[5];
 			a[5] = 30 / 0;
-		} catch (Exception e) {
+		} catch (ArithmeticException e) {
 			System.out.println("Arithmetic Exception occurs");
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("ArrayIndexOutOfBounds Exception occurs");
