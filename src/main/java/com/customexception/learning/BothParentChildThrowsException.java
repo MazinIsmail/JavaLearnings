d50f4230a6ca34ab5f7c348bc1dffa0ae316b4ce
SU332095 <SU332095@D-113155585.wipro.com>
2019-06-24 14:36:04 +0530

    exception

--- src/main/java/com/customexception/learning/BothParentChildThrowsException.java
+++ src/main/java/com/customexception/learning/BothParentChildThrowsException.java
@@ -1,20 +1,20 @@
 package com.customexception.learning;
 
 import java.io.IOException;
-//When base class and child class both throws a checked exception
+//When base class and child class both throws a checked/unchecked exception
  class Vehicle { 
 	   void brand() throws IOException
 	   {
 	       System.out.println("Yamaha");
 	   }  
 	}
-	class Bike extends Vehicle{
+	class BothParentChildThrowsException extends Vehicle{
 	    void brand() throws IOException
 	    {
 	        System.out.println("Bajaj");
 	    }  
 	    public static void main(String args[]){  
-	    	Vehicle obj = new Bike();  
+	    	Vehicle obj = new BothParentChildThrowsException();  
 	        try{
 		   obj.brand();
 		}catch(Exception e){
--- src/main/java/com/customexception/learning/OnlyChildClassThrowsUncheckedException.java
+++ src/main/java/com/customexception/learning/OnlyChildClassThrowsUncheckedException.java
@@ -1,19 +1,19 @@
 package com.customexception.learning;
-//If base class doesn’t throw any exception but child class throws an unchecked exception
+//If base class doesn’t throw any exception but child class can only throws an unchecked exception and not checked exception
 class Building {
 	void color()
 	   {
 	       System.out.println("Blue");
 	   }  
 	}
-	class Room extends Building{
+	class OnlyChildClassThrowsUncheckedException extends Building{
 	   //It throws an unchecked exception
 	   void color() throws NullPointerException
 	   {
 	       System.out.println("White");
 	   }  
 	   public static void main(String args[]){  
-		   Building obj = new Room();  
+		   Building obj = new OnlyChildClassThrowsUncheckedException();  
 	       obj.color(); 
 	   } 
 
