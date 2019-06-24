fec3850f14ed978439226a87e5c752dc02bb4a82
SU332095 <SU332095@D-113155585.wipro.com>
2019-06-24 12:58:41 +0530

    Exception Changes

---
+++ src/main/java/com/customexception/learning/OnlyChildClassThrowsCheckedException.java
@@ -0,0 +1,33 @@
+package com.customexception.learning;
+
+import java.io.*;
+//If base class doesn’t throw any exception but child class throws an checked exception
+class SuperClass { 
+	  
+    // SuperClass doesn't declare any exception 
+    void method()  
+    { 
+        System.out.println("SuperClass"); 
+    } 
+} 
+  
+// SuperClass inherited by the SubClass 
+class SubClass extends SuperClass { 
+  
+    // method() declaring Checked Exception IOException 
+    void method()  
+    { 
+  
+        // IOException is of type Checked Exception 
+        // so the compiler will give Error 
+  
+        System.out.println("SubClass"); 
+    } 
+  
+    // Driver code 
+    public static void main(String args[]) 
+    { 
+    	SuperClass s = new SubClass(); 
+        s.method(); 
+    } 
+} 
