8a6beb723274b34403a382135c03d9c8245d2bda
MazinIsmailW <MA20011300@APL-31R4KW2>
2020-10-05 11:39:41 +0530

    Updated Parallel Streams

--- src/main/java8/com/java8/learning/streamlearning/ParallelStreams.java
+++ src/main/java8/com/java8/learning/streamlearning/ParallelStreams.java
@@ -3,7 +3,22 @@ package com.java8.learning.streamlearning;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.IntStream;
 
+/**
+ * In a parallel stream, the operations are executed in parallel and there are
+ * two ways to create a parallel stream.
+ * 
+ * Using the parallelStream() method on a collection
+ * 
+ * Using the parallel() method on a stream
+ * 
+ * Stream implementation in Java is by default sequential unless until it is
+ * explicitly mentioned in parallel. When a stream executes in parallel, the
+ * Java runtime partitions the stream into multiple sub-streams. Aggregate
+ * operations iterate over and process these sub-streams in parallel and then
+ * combine the results.
+ */
 public class ParallelStreams {
 	public static void main(String[] args) {
 		int max = 1000000;
@@ -15,5 +30,12 @@ public class ParallelStreams {
 
 		long count = values.parallelStream().sorted().count();
 		System.out.println(count);
+
+		IntStream range = IntStream.rangeClosed(1, 10);
+		IntStream rangeParallel = range.parallel();
+		System.out.println("Stream running in parallel: " + rangeParallel.isParallel());
+		rangeParallel.parallel().forEach(x -> {
+			System.out.println("Thread : " + Thread.currentThread().getName() + ", value: " + x);
+		});
 	}
 }
