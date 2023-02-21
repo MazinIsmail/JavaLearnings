3a72325042d938644f81d3a89963c3428ccf8b24
0B7143649 <0B7143649@host.docker.internal>
2023-02-21 11:07:36 -0500

    Updated comments

--- src/main/designpattern/com/pattern/adapterpatterns/AdapterDesignPattern.java
+++ src/main/designpattern/com/pattern/adapterpatterns/AdapterDesignPattern.java
@@ -5,8 +5,14 @@ package com.pattern.adapterpatterns;
  * unrelated/uncommon interfaces to work together. In other words, the adapter
  * pattern makes two incompatible interfaces compatible without changing their
  * existing code.
+ * 
+ * The adapter pattern uses anti-corruption layer which allows communication
+ * between different systems that don't share same semantics. It can serves as
+ * adapter / facade layer that translate request from one subsystem to the other
+ * subsystem. It protects the application design to be independent from outside
+ * subsystem.
  */
-public class Main {
+public class AdapterDesignPattern {
 
 	public static void main(String[] args) {
 		Sparrow sparrow = new Sparrow();
